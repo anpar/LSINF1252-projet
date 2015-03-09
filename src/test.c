d3992271c1ceb518e4300e45955cad42aceb2ccb
anpar <paris.antoine.paris@gmail.com>
2015-03-09 13:52:36 +0100

    Add test for new_game

--- src/test.c
+++ src/test.c
@@ -12,6 +12,12 @@ int clean_suite(void) {return 0;}
 /* Test case functions */
 
 void test_new_game(void) {
+    struct game *game = new_game(10,10);
+    CU_ASSERT_PTR_NOT_NULL(game);
+    CU_ASSERT(game->xsize == 10);
+    CU_ASSERT(game->ysize == 10);
+    CU_ASSERT(game->cur_player == PLAYER_WHITE);
+    CU_ASSERT_PTR_NULL(game->moves);
 
 }
 
@@ -20,6 +26,16 @@ void test_load_game(void) {
 }
 
 void test_free_game(void) {
+    struct game *game = new_game(10,10);
+    free_game(game);
+}
+
+void test_push(void) {
+    struct move *list == NULL;
+
+}
+
+void test_pop(void) {
 
 }
 
@@ -57,6 +73,8 @@ int main(void) {
     if ((NULL == CU_add_test(pSuite, "test_new_game", test_new_game)) ||
         (NULL == CU_add_test(pSuite, "test_load_game", test_load_game)) ||
         (NULL == CU_add_test(pSuite, "test_free_game", test_free_game)) ||
+        (NULL == CU_add_test(pSuite, "test_push", test_push)) ||
+        (NULL == CU_add_test(pSuite, "test_pop", test_pop)) ||
         (NULL == CU_add_test(pSuite, "test_is_move_seq_valid", test_is_move_seq_valid))  ||
         (NULL == CU_add_test(pSuite, "test_apply_moves", test_apply_moves)) ||
         (NULL == CU_add_test(pSuite, "test_undo_moves", test_undo_moves))
@@ -67,8 +85,6 @@ int main(void) {
    }
 
     /* Run all tests using the CUnit Basic interface */
-    CU_basic_set_mode(CU_BRM_SILENT);
     CU_basic_run_tests();
-    CU_cleanup_registry();
     return CU_get_error();
 }
