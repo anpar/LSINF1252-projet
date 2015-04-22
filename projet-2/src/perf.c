7dd8708b5901668e1d1f85fe89581112b19dca4c
anpar <paris.antoine.paris@gmail.com>
2015-04-22 15:47:06 +0200

    Add functions definitions in core.h

--- projet-2/src/perf.c
+++ projet-2/src/perf.c
@@ -1,9 +1,6 @@
 #include <sys/times>
+#include "perf.c"
 
-/*
- * Return t2-t1 in microseconds 
- * Inspired by APE7. 
- */
 long timeval_diff(struct timeval *t2, struct timeval *t1)
 {
         long diff = (t2->tv_usec + 1000000 * t2->tv_sec) - (t1->tv_usec + 1000000 * t1->tv_sec);
